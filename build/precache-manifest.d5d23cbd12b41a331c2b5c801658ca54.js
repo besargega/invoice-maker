f7dc72454ff9c82d257d7d0a15bf51a0185e4e60
besigega62 <besigega62@gmail.com>
2023-03-01 14:53:00 +0100

    fix

---
+++ build/precache-manifest.d5d23cbd12b41a331c2b5c801658ca54.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee752d561d057ec617e9e66a01d42b45",
+    "url": "/invoice-maker/index.html"
+  },
+  {
+    "revision": "97b3b3111e38ab7bd11b",
+    "url": "/invoice-maker/static/css/2.903596ff.chunk.css"
+  },
+  {
+    "revision": "0cde5f9ca0019c5245a4",
+    "url": "/invoice-maker/static/css/main.9eeb420b.chunk.css"
+  },
+  {
+    "revision": "97b3b3111e38ab7bd11b",
+    "url": "/invoice-maker/static/js/2.a621ccb5.chunk.js"
+  },
+  {
+    "revision": "98ceb044c07d19cc68b4dbc8f9d25279",
+    "url": "/invoice-maker/static/js/2.a621ccb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cde5f9ca0019c5245a4",
+    "url": "/invoice-maker/static/js/main.d688145e.chunk.js"
+  },
+  {
+    "revision": "6d257acd0aa29c7db6b4",
+    "url": "/invoice-maker/static/js/runtime-main.3f332809.js"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/invoice-maker/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/invoice-maker/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "15c01d9b81ceb6702aa92e3b5416ce50",
+    "url": "/invoice-maker/static/media/remove.15c01d9b.svg"
+  },
+  {
+    "revision": "d4da359f65106dae13fb51495f035419",
+    "url": "/invoice-maker/static/media/resize.d4da359f.svg"
+  }
+]);
